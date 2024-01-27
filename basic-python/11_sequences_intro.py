22dd88f066128152c6a42cdd4a507d5892c53cb2
Samiul Azam <samiul.azam.cse@gmail.com>
2024-01-27 14:38:54 -0700

    sequence intro

---
+++ basic-python/11_sequences_intro.py
@@ -0,0 +1,24 @@
+text = """
+Sequences are 'ordered' collection of objects. Sets and dictionaries are not sequences, as
+objects into it are not ordered. There is not first element, seconds element or last element.
+
+Sequences use indexing to maintain the order. Index starts from 0, ends with (n - 1).
+Sequences are itself objects, they are container type objects (remember almost everything is object in python)
+
+Most common Python sequences are finite. However, there are some sequences that are infinite. However,
+its outside of the scope of this repo.
+
+Homogeneous sequence type: contains same types of objects. Example: [1,2,3,4,5]
+Heterogenous sequence type: contains different types of objects. Example: [1, 2.0, 'hello']
+
+Mutable: Allowed to change the state/data of an object
+Immutable: Not allowed to change the state/data of an object
+
+Basic sequence types in python:
+1. List: mutable + heterogenous
+2. Tuple: immutable + heterogenous
+3. Strings: immutable + homogenous
+"""
+
+print("Sequence Intro: ------- \n")
+print(text)
